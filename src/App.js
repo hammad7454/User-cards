f50b0b4ab3e307f340685c13f929107d6ef02ec4
Hammad Mustafa <mustafahammad745@gmal.com>
2025-08-07 13:51:53 +0500

    Fix import path casing for Vercel deployment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import logo from './logo.svg';
 import './App.css'; 
-import User from './components/User';
 import MultiActionAreaCard from './components/UserCard';
-import UserDialog from './components/UserDialog';
+
 
 function App() {
   return (
     <>
-    {/* <User/> */}
     <MultiActionAreaCard/>
-    {/* <UserDialog/> */}
     </>
   );
 }
